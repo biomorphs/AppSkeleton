046b55b3ea98cdf7a0e2df561128366986800704
biomorphs <matt.hoyle@gmail.com>
2015-07-06 05:11:23 +0200

    Test crap

--- src/main/app_skeleton.cpp
+++ src/main/app_skeleton.cpp
@@ -8,6 +8,7 @@
 #include "sde/render_system.h"
 #include "sde/asset_system.h"
 #include "sde/debug_render.h"
+#include <algorithm>
 
 enum class Materials : uint8_t
 {
@@ -65,16 +66,11 @@ uint8_t Sphere(const glm::vec3& voxelPosition, const glm::vec3& center, float ra
 	}
 }
 
-inline uint8_t Box(const glm::vec3& voxelPosition, const glm::vec3& boxMin, const glm::vec3& boxMax, uint8_t value)
+inline float Box(const glm::vec3& voxelPosition, const glm::vec3& boxMin, const glm::vec3& boxMax)
 {
-	if (glm::all(glm::greaterThanEqual(voxelPosition, boxMin)) && glm::all(glm::lessThanEqual(voxelPosition, boxMax)))
-	{
-		return value;
-	}
-	else
-	{
-		return static_cast<uint8_t>(Materials::Air);
-	}
+	const glm::vec3 bpos = (boxMax + boxMin) * 0.5f;
+	const glm::vec3 b = (boxMax - boxMin) * 0.5f;
+	return glm::length(glm::max(glm::abs(bpos - voxelPosition) - b, glm::vec3(0.0)));
 }
 
 struct ValueFiller
@@ -87,35 +83,28 @@ struct ValueFiller
 	}
 };
 
-struct BoxFiller
+inline bool Pillar(VoxelModel::ClumpDataAccessor& accessor, int x, int y, int z, const glm::vec3& vox, const glm::vec3& center)
 {
-	uint8_t m_value;
-	Math::Box3 m_box;
-	void operator()(VoxelModel::ClumpDataAccessor& accessor, glm::vec3 clumpOrigin, glm::vec3 voxelSize, glm::vec3 voxelCenter)
+	float pillar = Box(vox, center - glm::vec3(0.5f, 0.0f, 0.5f), center + glm::vec3(0.5f, 0.5f, 0.5f));
+	pillar = std::min(pillar, Box(vox, center - glm::vec3(0.25f, -0.5f, 0.25f), center + glm::vec3(0.25f, 7.5f, 0.25f)));
+	pillar = std::min(pillar, Box(vox, center - glm::vec3(0.5f, -7.5f, 0.5f), center + glm::vec3(0.5f, 8.0f, 0.5f)));
+	if (pillar <= 0.0f)
 	{
-		const Math::Box3 clumpBox(clumpOrigin, clumpOrigin + (voxelSize * 2.0f));
-		if (!m_box.Intersects(clumpBox))
+		if (Box(vox, center - glm::vec3(0.125f, 0.0f, 0.125f), center + glm::vec3(0.125f, 8.0f, 0.125f)) <= 0.0f)
 		{
-			return;
+			auto theClump = accessor.GetClump();
+			theClump->VoxelAt(x, y, z) = static_cast<uint8_t>(Materials::OuterWall);
+			return true;
 		}
-
-		auto theClump = accessor.GetClump();
-		for (int z = 0;z < 2;++z)
+		else
 		{
-			for (int y = 0;y < 2;++y)
-			{
-				for (int x = 0;x < 2;++x)
-				{
-					glm::vec3 vPos = VoxelPosition(clumpOrigin, voxelSize, voxelCenter, x, y, z);
-					if (Box(vPos, m_box.Min(), m_box.Max(), 1))
-					{
-						theClump->VoxelAt(x, y, z) = m_value;
-					}
-				}
-			}
+			auto theClump = accessor.GetClump();
+			theClump->VoxelAt(x, y, z) = static_cast<uint8_t>(Materials::Pillars);
+			return true;
 		}
 	}
-};
+	return false;
+}
 
 struct TestRoomBuilder
 {
@@ -131,41 +120,22 @@ struct TestRoomBuilder
 					uint8_t val = static_cast<uint8_t>(Materials::Air);
 
 					// first apply outer walls
-					uint8_t outerWall = Box(vPos, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.5f, 8.0f, 128.0f), 1);
-					outerWall += Box(vPos, glm::vec3(127.5f, 0.0f, 0.0f), glm::vec3(128.0f, 8.0f, 128.0f), 1);
-					outerWall += Box(vPos, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(128.0f, 8.0f, 0.5f), 1);
-					outerWall += Box(vPos, glm::vec3(0.0f, 0.0f, 127.5f), glm::vec3(128.0f, 8.0f, 128.0f), 1);
-					outerWall += Box(vPos, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(128.0f, 0.125f, 128.0f), 1);
-					if (outerWall != 0)
+					float outerWall = Box(vPos, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.5f, 8.0f, 128.0f));
+					outerWall = std::min(outerWall, Box(vPos, glm::vec3(127.5f, 0.0f, 0.0f), glm::vec3(128.0f, 8.0f, 128.0f)));
+					outerWall = std::min(outerWall, Box(vPos, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(128.0f, 8.0f, 0.5f)));
+					outerWall = std::min(outerWall, Box(vPos, glm::vec3(0.0f, 0.0f, 127.5f), glm::vec3(128.0f, 8.0f, 128.0f)));
+					outerWall = std::min(outerWall, Box(vPos, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(128.0f, 0.125f, 128.0f)));
+					if (outerWall <= 0.0f)
 					{
 						auto theClump = accessor.GetClump();
 						theClump->VoxelAt(x, y, z) = static_cast<uint8_t>(Materials::OuterWall);
 						continue;
 					}
 
-					// apply inner rooms as a simple grid
 					glm::vec3 wallSpacePos = glm::mod(vPos, glm::vec3(16.0f, 8.0f, 16.0f));
-					uint8_t innerWall = Box(wallSpacePos, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.5f, 8.0f, 16.0f), 1);
-					innerWall += Box(wallSpacePos, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(16.0f, 8.0f, 0.5f), 1);
-					if (innerWall != 0)
-					{
-						// subtract doors to inner walls
-						uint8_t isDoor = Box(wallSpacePos, glm::vec3(6.0f, 0.125f, 0.0f), glm::vec3(10.0f, 6.0f, 0.5f), 1);
-						isDoor += Box(wallSpacePos, glm::vec3(0.0f, 0.125f, 6.0f), glm::vec3(0.5f, 6.0f, 10.0f), 1);
-						if (!isDoor)
-						{
-							// apply 'steel' interior
-							uint8_t innerSteelWall = Box(wallSpacePos, glm::vec3(0.0f+0.125f, 0.0f, 0.0f), glm::vec3(0.25f + 0.125f, 8.0f, 16.0f), 1);
-							innerSteelWall += Box(wallSpacePos, glm::vec3(0.0f, 0.0f, 0.0f + 0.125f), glm::vec3(16.0f, 8.0f, 0.5f-0.125f), 1);
-
-							auto theClump = accessor.GetClump();
-							theClump->VoxelAt(x, y, z) = innerSteelWall != 0 ? static_cast<uint8_t>(Materials::OuterWall) : static_cast<uint8_t>(Materials::Walls);
-							continue;
-						}
-					}
 
 					// apply carpets to each inner room
-					if (Box(wallSpacePos, glm::vec3(2.0f, 0.125f, 2.0f), glm::vec3(14.0f, 0.25f, 14.0f), 1))
+					if (Box(wallSpacePos, glm::vec3(2.0f, 0.125f, 2.0f), glm::vec3(14.0f, 0.25f, 14.0f)) <= 0.0f)
 					{
 						auto theClump = accessor.GetClump();
 						theClump->VoxelAt(x, y, z) = static_cast<uint8_t>(Materials::Carpet);
@@ -173,31 +143,34 @@ struct TestRoomBuilder
 					}
 
 					// apply floor if no carpet
-					if (Box(wallSpacePos, glm::vec3(0.0f, 0.125f, 0.0f), glm::vec3(16.0f, 0.25f, 16.0f), 1))
+					if (Box(wallSpacePos, glm::vec3(0.0f, 0.125f, 0.0f), glm::vec3(16.0f, 0.25f, 16.0f)) <= 0.0f)
 					{
 						auto theClump = accessor.GetClump();
 						theClump->VoxelAt(x, y, z) = static_cast<uint8_t>(Materials::Floor);
 						continue;
 					}
 
-					// apply pillars every two rooms
-					glm::vec3 pillarSpacePos = glm::mod(vPos, glm::vec3(32.0f, 8.0f, 32.0f));
-					uint8_t pillar = Box(pillarSpacePos, glm::vec3(23.5f, 0.0f, 23.5f), glm::vec3(24.5f, 0.5f, 24.5f), 1);
-					pillar += Box(pillarSpacePos, glm::vec3(23.75f, 0.5f, 23.75f), glm::vec3(24.25f, 7.5f, 24.25f), 1);
-					pillar += Box(pillarSpacePos, glm::vec3(23.5f, 7.5f, 23.5f), glm::vec3(24.5f, 8.0f, 24.5f), 1);
-					if (pillar)
+					float innerWall = Box(wallSpacePos, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.5f, 8.0f, 16.0f));
+					innerWall = std::min(innerWall,Box(wallSpacePos, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(16.0f, 8.0f, 0.5f)));
+					if (innerWall <= 0.0f)
 					{
-						if (Box(pillarSpacePos, glm::vec3(23.75f+ 0.125f, 0.0f, 23.75f+ 0.125f), glm::vec3(24.25f- 0.125f, 8.0f, 24.25f- 0.125f), 1))
+						float door = Box(wallSpacePos, glm::vec3(6.0f, 0.25f, 0.0f), glm::vec3(10.0f, 6.0f, 0.5f));
+						door = std::min(door,Box(wallSpacePos, glm::vec3(0.0f, 0.125f, 6.0f), glm::vec3(0.5f, 6.0f, 10.0f)));
+						if (door > 0.0f)
 						{
+							float innerSteelWall = Box(wallSpacePos, glm::vec3(0.0f + 0.125f, 0.0f, 0.0f), glm::vec3(0.25f + 0.125f, 8.0f, 16.0f));
+							innerSteelWall = std::min(innerSteelWall, Box(wallSpacePos, glm::vec3(0.0f, 0.0f, 0.0f + 0.125f), glm::vec3(16.0f, 8.0f, 0.5f - 0.125f)));
+
 							auto theClump = accessor.GetClump();
-							theClump->VoxelAt(x, y, z) = static_cast<uint8_t>(Materials::OuterWall);
+							theClump->VoxelAt(x, y, z) = innerSteelWall <= 0.0f ? static_cast<uint8_t>(Materials::OuterWall) : static_cast<uint8_t>(Materials::Walls);
 						}
-						else
-						{
-							auto theClump = accessor.GetClump();
-							theClump->VoxelAt(x, y, z) = static_cast<uint8_t>(Materials::Pillars);
-						}
+						continue;
 					}
+
+					if(!Pillar(accessor, x, y, z, wallSpacePos, glm::vec3(2.5f,0.0f,2.5f)))
+						if(!Pillar(accessor, x, y, z, wallSpacePos, glm::vec3(13.5f, 0.0f, 2.5f)))
+							if(!Pillar(accessor, x, y, z, wallSpacePos, glm::vec3(2.5f, 0.0f, 13.5f)))
+								Pillar(accessor, x, y, z, wallSpacePos, glm::vec3(13.5f, 0.0f, 13.5f));
 				}
 			}
 		}
@@ -224,13 +197,17 @@ void AppSkeleton::InitialiseFloor(std::shared_ptr<Core::Asset>& materialAsset)
 	m_testFloor = std::make_unique<Floor>();
 	m_testFloor->Create(floorMaterials, glm::vec3(128.0f, 8.0f, 128.0f), 32);
 
+	double elapsed;
 	TestRoomBuilder valFiller;
-
+	{
+		Core::ScopedTimer time(elapsed);
 #ifdef SDE_DEBUG
-	m_testFloor->ModifyData(Math::Box3(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(16.0f, 8.0f, 16.0f)), valFiller);
+		m_testFloor->ModifyData(Math::Box3(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(16.0f, 8.0f, 16.0f)), valFiller);
 #else
-	m_testFloor->ModifyData(Math::Box3(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(128.0f, 8.0f, 128.0f)), valFiller);
+		m_testFloor->ModifyData(Math::Box3(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(128.0f, 8.0f, 128.0f)), valFiller);
 #endif
+	}
+	SDE_LOG("%f, %f", (float)elapsed, (float)elapsed * 1000.0f);
 }
 
 AppSkeleton::AppSkeleton()
@@ -263,6 +240,8 @@ bool AppSkeleton::PostInit()
 	m_forwardPassId = m_renderSystem->CreatePass("Forward");
 	m_renderSystem->GetPass(m_forwardPassId).GetCamera().SetClipPlanes(0.1f, 256.0f);
 	m_renderSystem->GetPass(m_forwardPassId).GetCamera().SetFOVAndAspectRatio(70.0f, 1280.0f / 720.0f);
+	m_renderSystem->DebugCamera().SetClipPlanes(0.1f, 256.0f);
+	m_renderSystem->DebugCamera().SetFOVAndAspectRatio(70.0f, 1280.0f / 720.0f);
 
 	// load material, on completion, create the floor
 	m_assetSystem->LoadAsset("simple_diffuse_material", [this](const std::string& asset, bool result)
@@ -319,6 +298,9 @@ bool AppSkeleton::Tick()
 		glm::vec3 intersectPoint = rayStart + ((rayEnd - rayStart) * tIntersect);
 		m_renderSystem->GetDebugRender().AddAxisAtPoint(intersectPoint);
 	}
+
+	m_renderSystem->GetDebugRender().AddAxisAtPoint(glm::vec3(0.0f));
+	m_renderSystem->GetDebugRender().AddAxisAtPoint(glm::vec3(4.0f));
 
 	return !m_quit;
 }
